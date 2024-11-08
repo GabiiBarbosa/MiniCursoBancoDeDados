7d591822b83417e1ecdc3191c81ee94e4b6d8bd5
GabiiBarbosa <gabriellebarbosa597@gmail.com>
2024-11-08 20:29:42 -0300

    Commit do projeto de banco de dados com python

---
+++ Usar_bd.py
@@ -0,0 +1,9 @@
+import DAO
+
+#saida = DAO.inserir_usuario("email@gmail.com", "gabi", "1234")
+#saida = DAO.inserir_usuario("email123@gmail.com", "Maria", "12345")
+
+#saida = DAO.buscar_usuario('gabi')
+#saida = DAO.listar_usuarios()
+#saida = DAO.atualizar_usuario(2, "João", "email123@gmail.com")
+print(saida)
